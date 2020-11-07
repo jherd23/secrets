2a443cf8286d63b70254b4e47f3d3c147f261480
jherd23 <jherd23@gmail.com>
2020-11-06 20:44:34 -0500

    Now serve port 80

--- secrets.js
+++ secrets.js
@@ -62,7 +62,7 @@ function drawSecret() {
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.set('view engine', 'pug');
 app.use(express.static('static'));
